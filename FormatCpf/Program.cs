ac14832d469b6d9fa3ca7c33fe084a9276a1be32
Felipe Neves de Arruda <50635026+FelipeDwD@users.noreply.github.com>
2020-02-21 13:08:30 -0300

    2020-02-21

---
+++ FormatCpf/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FormatCpf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpfNumerico = "52695148520";
+            Console.WriteLine("Cpf somente números: " + cpfNumerico);
+
+            string cpf = cpfNumerico.Substring(0, 3) + ".";
+            cpf += cpfNumerico.Substring(3, 3) + ".";
+            cpf += cpfNumerico.Substring(6, 3) + "-";
+            cpf += cpfNumerico.Substring(9, 2);
+
+            Console.WriteLine(cpf);
+
+        }
+    }
+}
